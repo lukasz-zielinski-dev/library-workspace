0d17f1219424b9035ef595f331edfdb4fc9137cb
Łukasz Zieliński <lukasz.zielinski.kontakt@gmail.com>
2021-05-08 21:34:41 +0200

    example library component

--- projects/my-library/src/lib/my-library.component.ts
+++ projects/my-library/src/lib/my-library.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-library',
   template: `
-    <p>
-      my-library works!
-    </p>
+    <div class="container">
+      <div class="welcome-card">{{ welcomeMessage }}</div>
+    </div>
   `,
   styles: [
-  ]
+    `
+      .welcome-card {
+        display: flex;
+        justify-content: center;
+      }
+      .welcome-card {
+        background-color: #ccffcc;
+        padding: 10px;
+        margin: 10px;
+        border-radius: 10px;
+        font-size: 24px;
+      }
+    `,
+  ],
 })
 export class MyLibraryComponent implements OnInit {
+  @Input() name: string;
+  welcomeMessage: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.welcomeMessage = `Hello ${this.name || 'Stranger'}!`;
   }
-
 }
